ec07bcf4ea197ecce13155f12ce7337012bde0aa
Morgan Bradford <morganjbradford95@gmail.com>
2021-03-22 13:29:27 -0500

    configure file
    Co-authored-by: Dani Renner <danijrenner@gmail.com>

--- Library/Models/DesignTimeDbContextFactory.cs
+++ Library/Models/DesignTimeDbContextFactory.cs
@@ -1,7 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
 namespace Library.Models
 {
-  public class ClassName
+  public class LibraryContextFactory : IDesignTimeDbContextFactory<LibraryContext>
   {
-    // properties, constructors, methods, etc. go here
+    LibraryContext IDesignTimeDbContextFactory<LibraryContext>.CreateDbContext(string[] args)
+    {
+      IConfigurationRoot configuration = new ConfigurationBuilder()
+        .SetBasePath(Directory.GetCurrentDirectory())
+        .AddJsonFile("appsettings.json")
+        .Build();
+      var builder = new DbContextOptionsBuilder<LibraryContext>();
+      builder.UseMySql(configuration["ConnectionStrings:DefaultConnection"],ServerVersion.AutoDetect(configuration["ConnectionStrings:DefaultConnection"]));
+      return new LibraryContext(builder.Options);
+    }
   }
 }
