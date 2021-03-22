e65dd926ba2cc3417e5880ea4f84310d0a88d019
Morgan Bradford <morganjbradford95@gmail.com>
2021-03-22 18:18:49 -0500

    correct type CodyId to CopyId
    Co-authored-by: Dani Renner <danijrenner@gmail.com>

--- Library/Models/Checkout.cs
+++ Library/Models/Checkout.cs
@@ -3,7 +3,7 @@ namespace Library.Models
   public class Checkout
   {
     public int CheckoutId { get; set; }
-    public int CodyId { get; set; }
+    public int CopyId { get; set; }
     public int PatronId { get; set; }
     public virtual Copy Copy { get; set; }
     public virtual Patron Patron { get; set; }
