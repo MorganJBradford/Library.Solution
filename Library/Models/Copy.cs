b6d4995a16ce45c627cfb5c877b545e177b8d2f3
Morgan Bradford <morganjbradford95@gmail.com>
2021-03-22 14:11:16 -0500

    add CopyId, BookId, JoinEntities properties
    Co-authored-by: Dani Renner <danijrenner@gmail.com>

--- Library/Models/Copy.cs
+++ Library/Models/Copy.cs
@@ -1,7 +1,17 @@
+using System.Collections.Generic;
+
 namespace Library.Models
 {
   public class Copy
   {
-    // properties, constructors, methods, etc. go here
+    public Copy()
+    {
+      JoinEntities = new HashSet<Checkout>();
+    }
+  
+
+    public int CopyId { get; set; }
+    public int BookId { get; set; }
+    public virtual ICollection<Checkout> JoinEntities { get; }
   }
 }
