bf472db1c18c204d03ca8984bc49a63a6f479abe
Morgan Bradford <morganjbradford95@gmail.com>
2021-03-22 13:55:30 -0500

    add id, title, checkou, duedate, AppUser, JoinEntities properties. Create constructor; set JoinEntities to hashset
    Co-authored-by: Dani Renner <danijrenner@gmail.com>

--- Library/Models/Book.cs
+++ Library/Models/Book.cs
@@ -1,7 +1,19 @@
+using System.Collections.Generic;
+
 namespace Library.Models
 {
   public class Book
   {
-    // properties, constructors, methods, etc. go here
+    public Book()
+    {
+      JoinEntities = new HashSet<AuthorBook>();
+    }
+    
+    public int BookId { get; set; }
+    public string Title { get; set; }
+    public DateTime CheckOut { get; set; }
+    public DateTime DueDate { get; set; }
+    public virtual ApplicationUser Librarian { get; set; }
+    public virtual ICollection<AuthorBook> JoinEntities { get; }
   }
 }
